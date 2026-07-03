f02f6472ca55912c829ec588fa024f14da42bed1
burakalt45 <altuntasburak095@gmail.com>
2026-07-04 01:42:44 +0300

    Set up Telegram bot with async main function

--- KankaBot-Ultimate/main.py
+++ KankaBot-Ultimate/main.py
@@ -1,1 +1,21 @@
+from telegram.ext import Application
 
+from config import BOT_TOKEN
+
+from database import init
+
+import asyncio
+
+async def startup(app):
+
+    await init()
+
+async def main():
+
+    app = Application.builder().token(BOT_TOKEN).post_init(startup).build()
+
+    app.run_polling()
+
+if __name__=="__main__":
+
+    asyncio.run(main())
